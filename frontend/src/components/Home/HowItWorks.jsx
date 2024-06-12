ec941b6e63bd04759982212106c38a91aed8246a
IT21831690 <110651507+IT21831690@users.noreply.github.com>
2024-06-12 09:23:27 +0530

    Initial commit

---
+++ frontend/src/components/Home/HowItWorks.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { FaUserPlus } from "react-icons/fa";
+import { MdFindInPage } from "react-icons/md";
+import { IoMdSend } from "react-icons/io";
+
+const HowItWorks = () => {
+  return (
+    <>
+      <div className="howitworks">
+        <div className="container">
+          <h3>How Carrer Sync Works</h3>
+          <div className="banner">
+            <div className="card">
+              <FaUserPlus />
+              <p>Create Account</p>
+              <p>
+                "Create an account with us to unlock exclusive features and
+                personalized recommendations tailored just for you. Sign up in
+                seconds and gain access to a world of opportunities. Let's get
+                started on your journey to success together."
+              </p>
+            </div>
+            <div className="card">
+              <MdFindInPage />
+              <p>Find a Job/Post a Job</p>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Consequuntur, culpa.
+              </p>
+            </div>
+            <div className="card">
+              <IoMdSend />
+              <p>Apply For Job/Recruit Suitable Candidates</p>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Consequuntur, culpa.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HowItWorks;
